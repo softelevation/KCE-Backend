1a5edf4929a8c9e16f57d4d11650381b71b0364a
Aman Tyagi <Aman@softelevation.com>
2022-12-08 05:38:34 -0700

    change singhup url

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -11,12 +11,12 @@ module.exports = function(app) {
   });
 
   app.post(
-    "/api/auth/signup",
+    '/api/authSign/signup',
     [
       verifySignUp.checkDuplicateUsernameOrEmail,
       // verifySignUp.checkRolesExisted
     ],
-    controller.signup
+    controller.signup,
   );
 
   app.post("/api/auth/signin", controller.signin);
